93ede2c8a64547590618c0788758172809a64013
Hassuni Salem <cph-ns156@cphbusiness.dk>
2019-03-12 11:32:41 +0100

    første commit - Nur

---
+++ src/main/java/entity/CityInfo.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Hassuni
+ */
+@Entity
+public class CityInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int zipCode;
+    private int city;
+    
+    
+
+    public CityInfo() {
+    }
+
+    public CityInfo(int id, int zipCode, int city) {
+        this.id = id;
+        this.zipCode = zipCode;
+        this.city = city;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public int getCity() {
+        return city;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setCity(int city) {
+        this.city = city;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CityInfo)) {
+            return false;
+        }
+        CityInfo other = (CityInfo) object;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entitys.CityInfo[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/entity/Person.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import entity.Address;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Hassuni
+ */
+@Entity
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String email;
+    private String fName;
+    private String lName;
+
+   @OneToMany(mappedBy = "Person")
+   private final List<Phone> phones = new ArrayList();
+   
+   @ManyToMany(cascade = CascadeType.ALL)
+   private final List<Hobby> hobbys = new ArrayList();
+   
+   @ManyToOne
+   private Address addresses;
+   
+    
+    public Person() {
+    }
+
+    public Person(int id, String email, String fName, String lName) {
+        this.id = id;
+        this.email = email;
+        this.fName = fName;
+        this.lName = lName;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Entitys.Person[ id=" + id + " ]";
+    }
+
+}
