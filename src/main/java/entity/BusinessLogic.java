ddbbd125303cdeaa3e3a50e81007a25ef3dc84f9
claudia <claudia@10.50.130.50>
2019-03-12 10:20:44 +0100

    First commit

---
+++ src/main/java/entity/BusinessLogic.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.List;
+
+/**
+ *
+ * @author claudia
+ */
+public interface BusinessLogic
+{
+
+    public Person getPerson(String phoneNumber);
+
+    public List<Person> getPersonsByHobby(Hobby hobby);
+
+    public List<Person> getPersonsByCity(String city);
+
+    public int getCountPersonByHobby(Hobby hobby);
+
+    public List<CityInfo> getAllzipCodes();
+
+    //CRUD
+    public Person addPerson(Person p);
+
+    public Person deletePerson(Person p);
+
+    public Person updatePerson(Person p);
+    
+}
